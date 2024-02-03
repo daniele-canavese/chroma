f2d5d190e091d1c3cdf5305eaea6ce9c7a98fc80
daniele-canavese <daniele.canavese@polito.it>
2024-02-03 18:33:16 +0100

    Minor fix

--- examples/progress.py
+++ examples/progress.py
@@ -18,11 +18,11 @@ for i in ProgressBar(range(1000), description="doing stuff..."):
 # Printing something while a progress bar is running.
 for i in ProgressBar(range(10)):
     pprint("iteration", i, highlight=True)
-    sleep(0.25)  # nosemgrep: arbitrary-sleep
+    sleep(0.25)  # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
 
 # Updating a table while a progress bar is running using the `table` parameter of the `ProgressBar` class.
 table = Table("what", "number", highlight=True)
 for i in ProgressBar(range(1000), table=table):
     if i % 42 == 0:
         table.add("random", random())
-    sleep(0.001)  # nosemgrep: arbitrary-sleep
+    sleep(0.001)  # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
