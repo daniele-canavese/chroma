b7c2e28f03fb998fd60ce93179292282de1d7c8a
daniele-canavese <daniele.canavese@polito.it>
2024-02-03 18:47:10 +0100

    Minor fix

--- examples/progress.py
+++ examples/progress.py
@@ -9,24 +9,24 @@ from chroma import ProgressBar, Table, pprint
 
 # A simple progress bar.
 for i in ProgressBar(range(1000)):
-    # nosemgrep: arbitrary-sleep
-    sleep(0.001)  # nosemgrep: arbitrary-sleep
+    # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
+    sleep(0.001)  # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
 
 # A progress bar with a description.
 for i in ProgressBar(range(1000), description="doing stuff..."):
-    # nosemgrep: arbitrary-sleep
-    sleep(0.001)  # nosemgrep: arbitrary-sleep
+    # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
+    sleep(0.001)  # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
 
 # Printing something while a progress bar is running.
 for i in ProgressBar(range(10)):
     pprint("iteration", i, highlight=True)
-    # nosemgrep: arbitrary-sleep
-    sleep(0.25)  # nosemgrep: arbitrary-sleep
+    # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
+    sleep(0.25)  # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
 
 # Updating a table while a progress bar is running using the `table` parameter of the `ProgressBar` class.
 table = Table("what", "number", highlight=True)
 for i in ProgressBar(range(1000), table=table):
     if i % 42 == 0:
         table.add("random", random())
-    # nosemgrep: arbitrary-sleep
-    sleep(0.001)  # nosemgrep: arbitrary-sleep
+    # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
+    sleep(0.001)  # nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep
